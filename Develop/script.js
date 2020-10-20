dd429b3cdaaff407b2ff18aeab1437a31863a53f
Donell Work <donelltorres@yahoo.com>
2020-10-20 12:18:21 -0400

    local storage functionality added and fixed button size/position

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
         console.log(newDesc);
         var taskTime = $(this).parent().attr('id');
         console.log(taskTime);
+
+        localStorage.setItem(taskTime, newDesc);
     });
 
     function updateTime() {
@@ -32,6 +34,17 @@ $(document).ready(function() {
     updateTime();
 
     var updateDelay = setInterval(updateTime, 10000);
+
+    $('#9 .description').val(localStorage.getItem('9'));
+    $('#10 .description').val(localStorage.getItem('10'));
+    $('#11 .description').val(localStorage.getItem('11'));
+    $('#12 .description').val(localStorage.getItem('12'));
+    $('#13 .description').val(localStorage.getItem('13'));
+    $('#14 .description').val(localStorage.getItem('14'));
+    $('#15 .description').val(localStorage.getItem('15'));
+    $('#16 .description').val(localStorage.getItem('16'));
+    $('#17 .description').val(localStorage.getItem('17'));
+
 
     $('#currentDay').text(moment().format('dddd, MMMM Do'));
 
